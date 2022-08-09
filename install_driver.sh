fd012484605c5f8d866919f43c2cf85ad6b56160
Jason Ernst <ernstjason1@gmail.com>
2022-08-08 21:13:48 -0700

    fixing the 20.04 21.30 version

--- install_driver.sh
+++ install_driver.sh
@@ -7,16 +7,18 @@ then
     then
       if [ $UBUNTU_VERSION == "20.04" ]
       then
-        dpkg --add-architecture i386 && \
-            apt-get update && apt-get -y dist-upgrade  && \
-            apt-get -y --no-install-recommends install xz-utils initramfs-tools
+        dpkg --add-architecture i386
+        apt-get update && apt-get -y dist-upgrade  && \
+        apt-get -y --no-install-recommends install ca-certificates curl xz-utils initramfs-tools
 
-        curl -o amdgpu.tar.xz --referer https://www.amd.com/en/support/kb/release-notes/rn-amdgpu-unified-linux-21-30 https://drivers.amd.com/drivers/linux/amdgpu-pro-21.30-1290604-ubuntu-20.04.tar.xz && \
-            tar xf amdgpu.tar.xz --strip-components 1 && \
-            ./amdgpu-install -y --opencl=rocr,legacy --headless --no-dkms && \
-            rm -rf /tmp/* && apt-get -y remove ca-certificates curl xz-utils initramfs-tools && \
-            apt-get clean autoclean && rm -rf /var/lib/{apt,dpkg,cache,log}
+        curl -o amdgpu.tar.xz --referer https://www.amd.com/en/support/kb/release-notes/rn-amdgpu-unified-linux-21-30 https://drivers.amd.com/drivers/linux/amdgpu-pro-21.30-1290604-ubuntu-20.04.tar.xz
+        tar xf amdgpu.tar.xz --strip-components 1
 
+        ./amdgpu-install -y --opencl=rocr,legacy --headless --no-dkms
+        rm -rf /tmp/*
+        apt-get -y remove ca-certificates curl xz-utils initramfs-tools
+        apt-get clean autoclean && rm -rf /var/lib/{apt,dpkg,cache,log}
+        
         exit
       else
         echo -e "UBUNTU_VERSION $UBUNTU_VERSION not supported with DRIVER_VERSION $DRIVER_VERSION"

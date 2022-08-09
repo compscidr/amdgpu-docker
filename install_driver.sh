65b5fc53065c64de92cb68e71ec2998c3c0b6ced
Jason Ernst <ernstjason1@gmail.com>
2022-08-08 21:29:12 -0700

    added wget

--- install_driver.sh
+++ install_driver.sh
@@ -9,7 +9,7 @@ then
       then
         dpkg --add-architecture i386
         apt-get update && apt-get -y dist-upgrade  && \
-        apt-get -y --no-install-recommends install ca-certificates curl xz-utils initramfs-tools
+        apt-get -y --no-install-recommends install ca-certificates curl xz-utils initramfs-tools wget
 
         curl -o amdgpu.tar.xz --referer https://www.amd.com/en/support/kb/release-notes/rn-amdgpu-unified-linux-21-30 https://drivers.amd.com/drivers/linux/amdgpu-pro-21.30-1290604-ubuntu-20.04.tar.xz
         tar xf amdgpu.tar.xz --strip-components 1
@@ -18,7 +18,6 @@ then
         rm -rf /tmp/*
         apt-get -y remove ca-certificates curl xz-utils initramfs-tools
         apt-get clean autoclean && rm -rf /var/lib/{apt,dpkg,cache,log}
-        
         exit
       else
         echo -e "UBUNTU_VERSION $UBUNTU_VERSION not supported with DRIVER_VERSION $DRIVER_VERSION"
